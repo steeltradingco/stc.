aa34b72acfc4c8aab2b1e8f47038da0caa07c4a1
steeltradingco <steeltrading.co01@gmail.com>
2024-09-01 22:45:39 +0500

    Create index.jsx

---
+++ src/views/MainPage/index.jsx
@@ -0,0 +1,15 @@
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+import Home from '../Home';
+
+function MainPage() {
+    return (
+        <>
+            <Navbar />
+            <Home />
+            <Footer />
+        </>
+    )
+};
+
+export default MainPage;
